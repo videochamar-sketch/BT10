cb243d158f614b1f6e64fb2f99a8af85b344a488
videochamar-sketch <videochamar@gmail.com>
2025-09-04 19:34:06 +0530

    Updated Stairs.jsx

--- src/components/common/Stairs.jsx
+++ src/components/common/Stairs.jsx
@@ -1,5 +1,5 @@
+import React, { useRef, useEffect } from 'react'
 import gsap from 'gsap'
-import { useRef, useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const Stairs = (props) => {
@@ -28,73 +28,52 @@ const Stairs = (props) => {
     tlRef.current = tl
 
     tl.to(stairParent, { display: 'block' })
-    tl.from(
-      stairParent.querySelectorAll('.stair'),
-      {
-        height: 0,
-        stagger: {
-          amount: -0.2
-        }
-      },
-      0
-    )
-    tl.to(
-      stairParent.querySelectorAll('.stair'),
-      {
-        y: '100%',
-        stagger: {
-          amount: -0.25
-        }
-      },
-      '+=0'
-    )
-    tl.to(stairParent, {
-      display: 'none'
-    })
-    tl.to(
-      stairParent.querySelectorAll('.stair'),
-      {
-        y: '0%'
-      },
-      '+=0'
-    )
-
-    // animate page in
-    gsap.fromTo(
-      page,
-      {
-        opacity: 0,
-        scale: 1.2
-      },
-      {
-        opacity: 1,
-        scale: 1,
-        duration: 0.8,
-        delay: 1.3,
-        ease: 'power2.out'
+    // animate each .stair element
+    const stairs = stairParent.querySelectorAll('.stair')
+    tl.from(stairs, {
+      height: 0,
+      stagger: {
+        amount: -0.2
       }
+    })
+    tl.to(stairs, {
+      y: '100%',
+      stagger: {
+        amount: -0.25
+      }
+    })
+    tl.to(stairParent, { display: 'none' })
+    tl.to(stairs, { y: '0%' })
+
+    // animate main page content fade/scale in
+    const pageTween = gsap.fromTo(
+      page,
+      { opacity: 0, scale: 1.2 },
+      { opacity: 1, scale: 1, duration: 0.8, delay: 1.3, ease: 'power2.out' }
     )
 
     return () => {
-      // cleanup timeline to avoid duplicates
+      // cleanup timeline and page tween
       if (tlRef.current) {
         tlRef.current.kill()
         tlRef.current = null
       }
+      if (pageTween && pageTween.kill) pageTween.kill()
     }
   }, [currentPath])
 
   return (
     <div>
-      <div ref={stairParentRef} className='h-screen w-full fixed z-20 top-0'>
-        <div className='h-full w-full flex'>
-          <div className='stair h-full w-1/5 bg-black'></div>
-          <div className='stair h-full w-1/5 bg-black'></div>
-          <div className='stair h-full w-1/5 bg-black'></div>
-          <div className='stair h-full w-1/5 bg-black'></div>
-          <div className='stair h-full w-1/5 bg-black'></div>
+      <div ref={stairParentRef} className="h-screen w-full fixed z-20 top-0">
+        <div className="h-full w-full flex">
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
+          <div className="stair h-full w-1/5 bg-black" />
         </div>
       </div>
+
       <div ref={pageRef} style={{ opacity: 1 }}>
         {props.children}
       </div>
